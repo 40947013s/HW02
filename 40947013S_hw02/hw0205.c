a39bb3306a075f4c2ae48af6ec1ecb53cd1dc3d9
40947013S <100991325+40947013s@users.noreply.github.com>
2022-03-13 03:45:36 +0000

    temp

--- 40947013S_hw02/hw0205.c
+++ 40947013S_hw02/hw0205.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 typedef struct _sVector
 {
@@ -20,12 +22,24 @@ typedef struct _sVector
 
 sVector * myvector_init()
 {
-    
+    sVector *res = (sVector *)calloc(1, sizeof(sVector *));
+    res->type = 0;
+    res->data.c.x = 0;
+    res->data.c.y = 0;
+    res->data.p.distance = 0;
+    res->data.c.angle = 0;
 }
 
 int myvector_set( sVector *pVector , uint8_t type, double a, double b )
 {
-    
+    double x, y, distance, angle; 
+    if(type != 1 || type != 2) return -1;
+    else if(type == 1) 
+    {
+        x = a, y = b;
+        distance = sqrt(pow(a, 2)+pow(b, 2));
+        
+    }
 }
 
 int main()
