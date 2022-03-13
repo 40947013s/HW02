9b4d1bb673487c5960402ccc54f9913a36be23d8
40947013S <100991325+40947013s@users.noreply.github.com>
2022-03-13 09:04:33 +0000

    update hw0205

--- 40947013S_hw02/hw0205.c
+++ 40947013S_hw02/hw0205.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 #include <stdlib.h>
+
+#define PI 3.14159265
+#define VAL 180.0/PI 
 
 typedef struct _sVector
 {
@@ -20,29 +24,37 @@ typedef struct _sVector
     } data;
 }sVector;
 
-sVector * myvector_init()
+sVector *myvector_init()
 {
     sVector *res = (sVector *)calloc(1, sizeof(sVector *));
     res->type = 0;
     res->data.c.x = 0;
     res->data.c.y = 0;
     res->data.p.distance = 0;
-    res->data.c.angle = 0;
+    res->data.p.angle = 0;
 }
 
 int myvector_set( sVector *pVector , uint8_t type, double a, double b )
 {
-    double x, y, distance, angle; 
-    if(type != 1 || type != 2) return -1;
+    if(type != 1 && type != 2) return -1;
     else if(type == 1) 
     {
-        x = a, y = b;
-        distance = sqrt(pow(a, 2)+pow(b, 2));
-        
+        /*double sum_square = pow(a, 2)+pow(b, 2);
+        pVector->data.p.distance = sqrt(sum_square);
+        pVector->data.p.angle = VAL * atan(a/b);*/
+        pVector->data.c.x = a, pVector->data.c.y = b;
     }
+    else
+    {
+        pVector->data.p.distance = a, pVector->data.p.angle = b/360.0;
+        /*pVector->data.c.x = a * cos((b/360.0)*VAL);
+        pVector->data.c.y = b * sin((b/360.0)*VAL);*/
+    }
+    return 0;
 }
 
 int main()
 {
-    
+    sVector *ans = myvector_init();
+    myvector_set(ans, 1, 3, 4);
 }
